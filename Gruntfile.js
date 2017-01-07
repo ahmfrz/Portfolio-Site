6dc8cdf1634c0600edee3b19635a77952c8ca2c3
ahmfrz <af.ahmfrz@gmail.com>
2017-01-07 10:02:17 +0530

    Added tasks
    Added comments and the following tasks:
    cssmin - for css minification
    watch - for live reload and running tasks automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,41 @@
 module.exports = function(grunt){
+    /* Load grunt tasks plugin makes sure that all required tasks
+    are loaded without the need of 'load npm tasks' */
     require('load-grunt-tasks')(grunt);
 
+    // Read the configuration file
     var config = grunt.file.readYAML("GruntConfig.yaml");
 
     grunt.initConfig({
+        // Concatinate javascript files
         concat:{
             dist:{
                 src:[config.jsSrcDir + "*"],
                 dest:config.jsDistDir + 'built.js'
             }
+        },
+
+        // Minify css files
+        cssmin:{
+            target:{
+                files:[{
+                    src:[config.cssSrcDir + "*"],
+                    dest: config.cssDistDir + 'main.min.css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        // Watch file changes
+        watch:{
+            files:[config.jsSrcDir + "*", config.cssSrcDir + "*"],
+            tasks:['concat', 'cssmin'],
+            options:{
+                livereload: true
+            }
         }
     });
 
-    grunt.registerTask('default', 'concat');
+    // Register all tasks
+    grunt.registerTask('default', 'concat', 'cssmin', 'watch');
 }
